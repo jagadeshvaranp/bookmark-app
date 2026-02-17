7952452f21d0e4d04ecf5c25f199f25d4d3dab8e
jagadeshvaran <jagadeshvaranperumal@gmail.com>
2026-02-17 19:38:11 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000/dashboard'
+        redirectTo: 'https://bookmark-app-kappa-livid.vercel.app/dashboard'
       }
     })
   }
